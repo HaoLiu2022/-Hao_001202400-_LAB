f5d8e32ea3066523e8ef75edafc2924860eeb2d1
Hao Liu <liu.hao5@northeastern.edu>
2023-01-22 17:18:39 -0500

    DONE

---
+++ Lab_01/src/model/ContactInfo.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author haoliu
+ */
+public class ContactInfo {
+    
+    private String phoneNum;
+    private String emailAddress;
+    
+    public ContactInfo() {
+        this.phoneNum = "";
+        this.emailAddress = "";
+    }
+
+    /**
+     * @return the phoneNum
+     */
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    /**
+     * @param phoneNum the phoneNum to set
+     */
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    /**
+     * @return the emailAddress
+     */
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    /**
+     * @param emailAddress the emailAddress to set
+     */
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }    
+    
+}
